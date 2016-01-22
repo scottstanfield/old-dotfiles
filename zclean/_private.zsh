5b5345905162f0dfa51c07f8f2c57c44fa24f247
Scott Stanfield <scott@vertigo.com>
2016-01-21 22:39:56 -0800

    removed unused

--- zclean/_private.zsh
+++
@@ -1,6 +0,0 @@
-export DOCKER_HOST=tcp://192.168.59.103:2376
-export DOCKER_CERT_PATH=/Users/scott/.boot2docker/certs/boot2docker-vm
-export DOCKER_TLS_VERIFY=1
-
-hash -d mmm=~/projects/e9/sqlmodels
-hash -d thor=~/projects/e9/repos/thorium
